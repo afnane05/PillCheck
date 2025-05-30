7580b873921c24df30a3e0d7a6c3abcb1fd92bf9
taha-tajmout <ta.tajmout@gmail.com>
2025-05-30 15:31:23 +0100

    database changed

--- dataBase/pillcheck_database.sql
+++ dataBase/pillcheck_database.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: May 25, 2025 at 01:59 PM
+-- Generation Time: May 30, 2025 at 02:20 PM
 -- Server version: 9.1.0
 -- PHP Version: 8.3.14
 
@@ -29,7 +29,8 @@ SET time_zone = "+00:00";
 
 DROP TABLE IF EXISTS `patient`;
 CREATE TABLE IF NOT EXISTS `patient` (
-  `cin` varchar(20) NOT NULL,
+  `id` int NOT NULL AUTO_INCREMENT,
+  `cin` varchar(20) DEFAULT NULL,
   `nom` varchar(100) DEFAULT NULL,
   `prenom` varchar(100) DEFAULT NULL,
   `date_naissance` date DEFAULT NULL,
@@ -37,8 +38,23 @@ CREATE TABLE IF NOT EXISTS `patient` (
   `etat` varchar(50) DEFAULT NULL,
   `sexe` varchar(20) DEFAULT NULL,
   `traitement` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`cin`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `user_id` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `patient`
+--
+
+INSERT INTO `patient` (`id`, `cin`, `nom`, `prenom`, `date_naissance`, `telephone`, `etat`, `sexe`, `traitement`, `user_id`) VALUES
+(3, '88888', 'Merzak', 'Romaissae', '2025-05-06', '66666', 'me3gaza', 'Femme', 'Spasfon', 1),
+(4, '33333333', 'Tajmout', 'fatima', '2025-05-02', '0000000', 'Lbsala', 'Homme', 'Spasfon', 1),
+(5, '9999', 'elkkhayir', 'samira', '2025-05-19', '55555', 'lmoooot', 'Femme', 'Spasfon', 1),
+(6, '9999999', 'bouaaza', 'Hfissa', '2025-05-07', '7777', 'cuteness', 'Femme', 'Spasfon', 1),
+(7, '555555555', 'sadouki', 'meryem', '2025-07-24', '777777', 'Ne3asa', 'Femme', 'Doliprane', 1),
+(11, '33333333', 'EEEEEEEE', 'EEEEE', '2025-05-21', '888888888', 'BBBB', 'Femme', 'Doliprane', 0),
+(9, '88888888', 'bellafkih', 'safae', '2005-01-15', '0000000000', 'migraine', 'Femme', 'Spasfon', 1);
 
 -- --------------------------------------------------------
 
@@ -48,13 +64,13 @@ CREATE TABLE IF NOT EXISTS `patient` (
 
 DROP TABLE IF EXISTS `rdv`;
 CREATE TABLE IF NOT EXISTS `rdv` (
-  `ID_RDV` int NOT NULL,
+  `id` int NOT NULL AUTO_INCREMENT,
   `DATE_RDV` datetime DEFAULT NULL,
   `MOTIF_RDV` varchar(30) DEFAULT NULL,
   `STATUT` enum('confirmé','en attente','annulée') DEFAULT NULL,
-  `CIN` varchar(7) DEFAULT NULL,
-  PRIMARY KEY (`ID_RDV`),
-  KEY `CIN` (`CIN`)
+  `user_id` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
@@ -65,7 +81,8 @@ CREATE TABLE IF NOT EXISTS `rdv` (
 
 DROP TABLE IF EXISTS `traitement`;
 CREATE TABLE IF NOT EXISTS `traitement` (
-  `NOM_TRAITEMENT` varchar(20) NOT NULL,
+  `id` int NOT NULL AUTO_INCREMENT,
+  `NOM_TRAITEMENT` varchar(20) DEFAULT NULL,
   `TYPE_Traitement` varchar(20) DEFAULT NULL,
   `MALADIE` varchar(30) DEFAULT NULL,
   `DESCRIPTION` varchar(100) DEFAULT NULL,
@@ -75,10 +92,18 @@ CREATE TABLE IF NOT EXISTS `traitement` (
   `POSOLOGIE` varchar(10) DEFAULT NULL,
   `EFFETS_SECONDAIRES` varchar(100) DEFAULT NULL,
   `ETAT` enum('en cours','suspendue','terminé') DEFAULT NULL,
-  `cin` varchar(7) DEFAULT NULL,
-  PRIMARY KEY (`NOM_TRAITEMENT`),
-  KEY `cin` (`cin`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `user_id` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `traitement`
+--
+
+INSERT INTO `traitement` (`id`, `NOM_TRAITEMENT`, `TYPE_Traitement`, `MALADIE`, `DESCRIPTION`, `DATE_DEBUT`, `DATE_FIN`, `DUREE_ESTIMEE`, `POSOLOGIE`, `EFFETS_SECONDAIRES`, `ETAT`, `user_id`) VALUES
+(1, 'zzzzzzz', 'zzzzzzz', 'zzzzzzzz', 'zzzzzz', '2025-05-29', '2025-06-05', 'zzzzzz', 'zzzzz', 'zzzzzzzz', 'suspendue', 1),
+(2, 'PPPPPPP', 'GGGGGGG', 'GGGGGGGG', 'GGGGGGG', '2025-05-30', '2025-06-06', 'GGGGGG', 'GGGGGGG', 'GGGGGGG', 'suspendue', 3);
 
 -- --------------------------------------------------------
 
@@ -88,18 +113,20 @@ CREATE TABLE IF NOT EXISTS `traitement` (
 
 DROP TABLE IF EXISTS `utilisateur`;
 CREATE TABLE IF NOT EXISTS `utilisateur` (
-  `nom` varchar(10) NOT NULL,
-  `email` varchar(20) NOT NULL,
-  `motDePasse` varchar(50) NOT NULL,
-  PRIMARY KEY (`nom`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nom` varchar(100) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `motDePasse` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Dumping data for table `utilisateur`
 --
 
-INSERT INTO `utilisateur` (`nom`, `email`, `motDePasse`) VALUES
-('afnane', 'afnane@gmail.com', 'afnanecoco');
+INSERT INTO `utilisateur` (`id`, `nom`, `email`, `motDePasse`) VALUES
+(1, 'afnane', 'afnane@gmail.com', 'afnanecoco');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
